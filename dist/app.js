c69bdd70f5827dee79a05db3cca376422bed8abd
Muhammad Farraos Abdillah <zqr.farraosohaada@gmail.com>
2024-12-21 04:37:12 +0700

    1.3 fix be

--- dist/app.js
+++ dist/app.js
@@ -41,6 +41,7 @@ const cors_1 = __importDefault(require("cors"));
 const path_1 = __importDefault(require("path"));
 const config_1 = require("./config");
 const product_router_1 = require("./routers/product.router");
+console.log('DB_URL:', process.env.DB_URL);
 class App {
     constructor() {
         this.app = (0, express_1.default)();
